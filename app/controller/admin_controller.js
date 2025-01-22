36c873321cdfeb820e90da3fc4c49111e5bf7fb2
Damien Chabanier <dam31vt@gmail.com>
2025-01-22 14:05:01 +0100

    formulaire d'ajout de café créé, on peut maintenant ajouter un café si on est admin

--- app/controller/admin_controller.js
+++ app/controller/admin_controller.js
@@ -12,7 +12,7 @@ const adminController = {
 
     const admin = await dataMapper.admin(userCo.name, userCo.password);
 
-    if (userCo.name === "" || userCo.password === "") {
+    if ((userCo.name === "") === "" || userCo.password === "") {
       res.render("admin.ejs", {
         errorMessage: "veuillez remplir tous les champs",
       });
@@ -35,22 +35,22 @@ const adminController = {
   },
   async AddCoffee(req, res) {
     const coffee = req.body;
-
-    // const admin = await dataMapper.addCoffe(coffee);
-
-    // if () {
-    //   res.render("admin.ejs", {
-    //     errorMessage: "veuillez remplir tous les champs",
-    //   });
-    // } else if (admin === "" || admin === undefined) {
-    //   res.render("admin.ejs", {
-    //     errorMessage: "Mot de passe ou nom d'utilisateur incorrect",
-    //   });
-    // } else {
-    //   req.session.admin = [];
-    //   req.session.admin = admin;
-    //   res.redirect("/admin/addCoffee");
-    // }
+    if (
+      coffee.nom === "" ||
+      coffee.description === "" ||
+      coffee.reference === "" ||
+      coffee.origine === "" ||
+      coffee.prix_kilo === "" ||
+      coffee.caracteristique === ""
+    ) {
+      res.render("addCoffe.ejs", {
+        adminConnected: req.session.admin,
+        errorMessage: "veuillez remplir tous les champs",
+      });
+    } else {
+      dataMapper.addCoffe(coffee);
+      res.redirect(`/produit/${coffee.reference}`);
+    }
   },
   logout(req, res) {
     req.session.admin = [];
